196e481be4765798a1b77c6d9c2dcb19bcbde6c0
pearpages <pere@soms.cat>
2017-09-16 22:46:30 +0200

    mistakes with subjects

---
+++ src/common-mistakes/subjects.js
@@ -0,0 +1,30 @@
+// instead of (DO NOT)
+const click$ = new Rx.Subject();
+
+document.addEventListener('click', function (ev) {
+    click$.next(ev);
+});
+
+click$.subscribe(function (ev) {
+    console.log(ev);
+});
+
+// DO
+
+const click$ = Rx.Observable.create(function subscribe(observer) {
+    const listener = function (ev) {
+        observer.next(ev);
+    };
+
+    document.addEventListener('click', listener);
+
+    return function unsubscribe() {
+        document.removeEventListener('click', listener);
+    };
+});
+
+const subscription = click$.subscribe(x => console.log(x));
+
+setTimeout(function () {
+    subscription.unsubscribe();
+}, 4000);
