6a550460203023f0bdd3434ee834afa9a0886e41
pearpages <pere@soms.cat>
2017-09-25 19:07:53 +0200

    windowToggle

--- src/high-order-observables/windowToggle.js
+++ src/high-order-observables/windowToggle.js
@@ -1,1 +1,9 @@
-// TODO
+const click$ = Rx.Observable.fromEvent(document, 'click');
+const clock$ = Rx.Observable.interval(1000);
+const down$ = Rx.Observable.fromEvent(document, 'mousedown');
+const up$ = Rx.Observable.fromEvent(document, 'mouseup');
+
+const result$ = clock$
+    .windowToggle(down$, () => up$)
+    .mergeAll()
+    .subscribe(x => console.log(x));
